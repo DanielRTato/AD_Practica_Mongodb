e889542575094828f6293d9989bca7dbd7643bff
Daniel Rodriguez <drodrigueztato@danielcastelao.org>
2025-12-12 14:28:50 +0100

    Añadir services

--- src/main/java/org/example/service/AdestradorService.java
+++ src/main/java/org/example/service/AdestradorService.java
@@ -1,4 +1,27 @@
 package org.example.service;
 
+import org.example.model.Adestrador;
+import org.example.repository.AdestradorRepository;
+
+import java.util.List;
+
 public class AdestradorService {
+
+    private final AdestradorRepository adestradorRepo;
+
+    public AdestradorService(AdestradorRepository adestradorRepo) {
+        this.adestradorRepo = adestradorRepo;
+    }
+
+    public void crearAdestrador(Adestrador adestrador) {
+        adestradorRepo.save(adestrador);
+    }
+
+    public Adestrador buscarAdestrador(String id) {
+        return adestradorRepo.findById(id).orElse(null);
+    }
+
+    public List<Adestrador> buscarAdestradores() {
+        return adestradorRepo.findAll();
+    }
 }
--- src/main/java/org/example/service/PokemonService.java
+++ src/main/java/org/example/service/PokemonService.java
@@ -1,4 +1,31 @@
 package org.example.service;
 
+import org.example.model.Adestrador;
+import org.example.model.Pokemon;
+import org.example.repository.AdestradorRepository;
+import org.example.repository.PokemonRepository;
+
+import java.util.List;
+
 public class PokemonService {
+    private final PokemonRepository pokemonRepo;
+    private final AdestradorRepository adestradorRepo;
+
+
+    public PokemonService(PokemonRepository pokemonRepo, AdestradorRepository adestradorRepo) {
+        this.pokemonRepo = pokemonRepo;
+        this.adestradorRepo = adestradorRepo;
+    }
+
+    private void crearPokemon(Pokemon pokemon) {
+        pokemonRepo.save(pokemon);
+    }
+
+    public Pokemon buscarPokemon(String id) {
+        return pokemonRepo.findById(id).orElse(null);
+    }
+
+    public List<Pokemon> buscarPokemons() {
+        return pokemonRepo.findAll();
+    }
 }
